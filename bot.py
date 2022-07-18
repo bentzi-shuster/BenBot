cd0c53b4235fb9f0c294b8e8233ed0076a4ac1bb
bentzi-shuster <bentzishuster613@gmail.com>
2022-07-17 22:17:25 -0400

    no prints

--- bot.py
+++ bot.py
@@ -21,13 +21,6 @@ async def on_ready():
     for guild in client.guilds:
         if guild.name == GUILD:
             break
-
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
-
-
 @client.event
 async def on_message(message):
     # 5% chance of responding to a message
@@ -38,12 +31,10 @@ async def on_message(message):
         keywords = kw_extractor.extract_keywords(msg)
         longest_phrase = max(keywords, key=lambda x: len(x[0]))
         longest_phrase = longest_phrase[0]
-        print(longest_phrase)
         search_term = longest_phrase
         r = requests.get("https://tenor.googleapis.com/v2/search?q=%s&key=%s&client_key=%s&limit=%s" % (search_term, apikey, ckey,  lmt))
         if r.status_code == 200:
             top_gif = json.loads(r.content)
-            print(top_gif)
             messageURLGIF = top_gif['results'][0]['media_formats']
             messageURLGIF = messageURLGIF['gif']['url']
             await message.channel.send(messageURLGIF)
